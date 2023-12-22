e1e748e83b4f993f21e0288035b79b78b0027461
viteky <jayden.vitek@gmail.com>
2023-12-23 10:04:18 +1100

    initial commit

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
+from helpers import login_required
+
+app = Flask(__name__)
+
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+
+@app.route("/")
+@login_required
+def index():
+    return render_template("index.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
